4615cc4bb487e58aafadb700b936c8201d0990fd
jake <>
2021-12-04 23:21:14 -0600

    updating app title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div>
       <header className="bg-light border-bottom py-2">
         <div className="container">
-          <h2 className="mb-0">Emergent Sample App</h2>
+          <h2 className="mb-0">Emergent Software</h2>
         </div>
       </header>
       <div className="container py-4">
